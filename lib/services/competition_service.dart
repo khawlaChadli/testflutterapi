e52357d04b2a0dcca3f8cad15c8351062e0960fc
khawla <chadlikhawla0@gmail.com>
2022-07-26 10:08:28 +0200

    getCompetitionsId

--- lib/services/competition_service.dart
+++ lib/services/competition_service.dart
@@ -18,4 +18,28 @@ class CompetitionService {
           .toList();
     }
   }
+
+  Future<Competition?> getCompetitionsId(competitionId) async {
+    final jsonResponse;
+    print(competitionId);
+    var client = http.Client();
+    var uri =
+        Uri.parse('https://api-d.racegorilla.com/competition/$competitionId');
+    var response = await client.get(uri);
+    // //var response = await http.get(
+    // //Uri.https('https://api-d.racegorilla.com/competition/', competitionId));
+    print(response);
+
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      jsonResponse = jsonData['Data'];
+      print(jsonResponse);
+      return Competition.fromJson(jsonResponse);
+    }
+    // print(jsonResponse.length);
+    //return jsonResponse.map((competitionId) => Competition.fromJson(competitionId)).toList();
+    //return null;
+  }
+
+  //}
 }
