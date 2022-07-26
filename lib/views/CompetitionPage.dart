a3d61997b6a3f470013f8137f49362a5d31a5ee5
khawla <chadlikhawla0@gmail.com>
2022-07-26 10:09:43 +0200

    StatefulWidget+getData

--- lib/views/CompetitionPage.dart
+++ lib/views/CompetitionPage.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:testflutterapi/models/competition.dart';
+import 'package:testflutterapi/services/competition_service.dart';
 
-class CompetitionPage extends StatelessWidget {
+class CompetitionPage extends StatefulWidget {
   const CompetitionPage({super.key, required this.competitionId});
 
   final int competitionId;
+
+  @override
+  State<CompetitionPage> createState() => _CompetitionPageState();
+}
+
+class _CompetitionPageState extends State<CompetitionPage> {
+  Competition? competition;
+  var isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    competition =
+        await CompetitionService().getCompetitionsId(widget.competitionId);
+    if (competition != null) {
+      setState(() {
+        isLoaded = true;
+      });
+      print(competition);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +45,7 @@ class CompetitionPage extends StatelessWidget {
                 bottomRight: Radius.circular(70),
                 bottomLeft: Radius.circular(70))),
         title: Text(
-          "The Competition $competitionId ",
+          "The Competition ${widget.competitionId} ",
           textAlign: TextAlign.center,
         ),
       ),
@@ -27,7 +54,7 @@ class CompetitionPage extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text('ok'),
+          child: Text(competition?.Name ?? 'ok'),
         ),
       ),
     );
