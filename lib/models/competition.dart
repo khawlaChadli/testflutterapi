f157d214daaa48e806e3f0aecd74e02025982dc4
khawla <chadlikhawla0@gmail.com>
2022-08-08 08:27:31 +0200

    Add Status, Sport

--- lib/models/competition.dart
+++ lib/models/competition.dart
@@ -4,12 +4,19 @@ class Competition {
   final int CompetitionId;
   final String Name;
   final String Location;
+  final String Status;
+  // final String Type;
+  final String Sport;
+
   final int StartDate;
 
   const Competition({
     required this.CompetitionId,
     required this.Name,
     required this.Location,
+    required this.Status,
+    // required this.Type,
+    required this.Sport,
     required this.StartDate,
   });
 
@@ -18,9 +25,13 @@ class Competition {
       CompetitionId: json['CompetitionId'],
       Name: json['Name'],
       Location: json['Location'],
+      Status: json['Status'] ?? '',
+      // Type: json['Type'],
+      Sport: json['Sport'],
+
       StartDate: json['StartDate'],
     );
   }
 
-  String? get title => null;
+  // String? get title => null;
 }
